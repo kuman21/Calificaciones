df72098a3991cdb8a5d189c6ddfefc569cf97125
Victor Tovar <41172040+kuman21@users.noreply.github.com>
2021-05-29 10:24:53 -0500

    Update register.blade.php
    Se cambio el texto a español en la pantalla de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,26 +10,26 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-label for="name" value="Nombre" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="Correo Electrónico" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="Contraseña" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-label for="password_confirmation" value="Confirmar Contraseña" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+            {{--@if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
                     <x-jet-label for="terms">
                         <div class="flex items-center">
@@ -44,15 +44,15 @@
                         </div>
                     </x-jet-label>
                 </div>
-            @endif
+            @endif--}}
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    ¿Ya registrado?
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    Registrar
                 </x-jet-button>
             </div>
         </form>
