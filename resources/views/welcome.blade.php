f1e57c8ed60213cecbc27434064a2c751cb2c650
Victor Tovar <41172040+kuman21@users.noreply.github.com>
2021-05-29 11:47:12 -0500

    Update welcome.blade.php
    Se añadio el nombre del profesor.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,9 @@
                                     </button>
                                 </div>
                             </form>
+                            <p class="text-center lg:text-left mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                Prof. Víctor Tovar Ríos
+                            </p>
                         </div>
                     </main>
                 </div>
